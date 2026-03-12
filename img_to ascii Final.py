fb8fcd540e7c338de6ded442838bd50dd4eeb400
Maisy <55251184+max9676867@users.noreply.github.com>
2026-03-12 20:48:22 +0100

    google style docstrings
    made the docstring using the standart google style docstring and translated every variable in english.

--- img_to ascii Final.py
+++ img_to ascii Final.py
@@ -1,44 +1,74 @@
-def grid_img(gray_image):
-    '''asks the user a set amount of columns and calculates the right number of lines accounting for the ratio of the image
-    input : grayscale of the image
-    output : number of columns, lines, size of a case and size of the image'''
+import cv2
+import numpy
+import os
+
+
+def get_grid_parameters(gray_image : numpy.ndarray):
+    """prompts the user for the number of columns and calculates the number of rows,
+    maintaining the image's aspect ratio. Also computes the size of each grid cell.
     
-    colonnes = int(input("number of columns : "))
-    H, L = gray_image.shape[:2] # image dimension
-    ratio = L/H
-    lignes = int(colonnes/ratio/2)
-    cellules_colonnes = L / colonnes 
-    cellules_lignes = H / lignes 
+    Args : 
+        gray_image (ndarray): grayscale image 
+    
+    Returns :
+        tuple [int, int, float, float, int, int]:
+            - number of columns
+            - number of rows
+            - width of each cell
+            - height of each cell
+            - total width of the image
+            - total height of the image
+    """
+    
+    columns = int(input("number of columns : "))
+    Height, Width = gray_image.shape[:2] 
+    ratio = Width / Height
+    rows = int(columns / ratio / 2)
+    cell_width = Width / columns
+    cell_height = Height / rows
 
-    return (colonnes, lignes, cellules_colonnes, cellules_lignes, H, L)
+    return columns, rows, cell_width, cell_height, Width, Height
 
-def obt_clarte(gray_image):
+def compute_grid_brightness(gray_image):
+    """divides the image into a grid and computes the average brightness for each cell.
+    
+    Args : 
+        gray_image (ndarray): grayscale image
+    
+    Returns :
+        np.ndarray : 2D array of average brightness values for each grid cell."""
     '''creates a grid with a set number of column and calculates the gray level of each case of the image.
     input : grayscale of an image
     return : list storing the gray level of each case'''
-    colonnes, lignes, cell_col, cell_lign, H, L = grid_img(gray_image)
-    clarte_grille = numpy.zeros((lignes, colonnes), dtype=float)
+    columns, rows, cell_width, cell_height, Width, Height = get_grid_parameters(gray_image)
+    brightness_grid = numpy.zeros((rows, columns), dtype=float)
     
-    for i in range(lignes):
-        for j in range(colonnes):
+    for i in range(rows):
+        for j in range(columns):
             # image cropping into identical squares 
-            y1 = int(i * cell_lign)
-            y2 = int((i + 1) * cell_lign) if i < lignes - 1 else H
-            x1 = int(j * cell_col)
-            x2 = int((j + 1) * cell_col) if j < colonnes - 1 else L
+            y1 = int(i * cell_height)
+            y2 = int((i + 1) * cell_height) if i < rows - 1 else Height
+            x1 = int(j * cell_width)
+            x2 = int((j + 1) * cell_width) if j < columns - 1 else Width
             
             cell = gray_image[y1:y2, x1:x2]
             # gray level of each case
-            clarte_grille[i, j] = float(cv2.mean(cell)[0])
+            brightness_grid[i, j] = float(cv2.mean(cell)[0])
     
-    return clarte_grille
+    return brightness_grid
 
-def ASCII(grille):
-    '''transforms the list of gray level in ascii characters and prints the result
-    input : list of gray level
-    return : list of the ascii art'''
+def convert_to_ASCII(brightness_grid):
+    """converts the brightness grid into ASCII art using predefined characters scale.
+    
+    Args :
+        brightness_grid (np.ndarray): 2d array of brightness values
+        
+    Returns :
+        List [str]: list of strings, each representing a line of the ASCII art.
+    """
 
-    ### exemples of lists for the render ###
+    ###### exemples of lists for the render ######
+    # # darker to lighter # #
     #Asciiscale = ["@", "%", "#", "*", "+", "=", "-", ":", ".", " "]
     #Asciiscale = ["$","@","B","%","8","&","W","M","#","*","o","a","h","k","b","d","p","q","w","m","Z","O","0","Q","L","C","J","U","Y","X","z","c","v","u","n","x","r","j","f","t","/",'\ ','|',"(",")","1","{","}","[","]","?","-","_","+","~","i","!","l","I",";",":",",","\ ","^","`",'"'," "]
     #Asciiscale = ["@", "#", "S", "%", "?", "*", "+", ";", ":", "."," "]
@@ -48,44 +78,47 @@ def ASCII(grille):
 
     aaaaa = []
     
-    # conversion taux de gris en ascii
-    for i in range(len(grille)):
-        ligne = ""
-        for j in range(len(grille[0])):
-            val = int(grille[i][j]/255 * (len(Asciiscale) - 1))
-            ligne += Asciiscale[val]
-        aaaaa.append(ligne)
+    for i in range(len(brightness_grid)):
+        line = ""
+        for j in range(len(brightness_grid[0])):
+            val = int(brightness_grid[i][j]/255 * (len(Asciiscale) - 1))
+            line += Asciiscale[val]
+        aaaaa.append(line)
 
-    # display of ASCII art
-    for k in range(len(aaaaa)):
-        print(aaaaa[k])
+    for line in aaaaa:
+        print(line)
     
     return aaaaa
 
-def save_ASCII (ASCII) :
-    '''saves the ascii art as a .txt file in the user's dowload file
-    input : string (the ascii art to save)'''
+def save_ASCII (ASCII_art) :
+    """Saves the ASCII art as a text file in the user's Downloads directory.
+    
+    Args :
+        ASCII_art (List[str]): list of strings representing the ASCII art
+    """
     dl_path = os.path.join(os.path.expanduser("~"), "Downloads")
     output_file = os.path.join(dl_path, "ascii_image.txt")
+
     with open(output_file, "w", encoding="utf-8") as f:
-        for line in ASCII:
+        for line in ASCII_art:
             f.write(line + "\n")
 
-###########################################################
-import cv2
-import numpy
-import os
+
+def main():
+    """
+    Mains function to load the image, converts it to ASCII art and saves the result."""
+    ###### Load file here ######
+    image = cv2.imread("c:/Users/Maxime/Desktop/projets/vache.jpg")
+
+    if image is None:
+        raise ValueError("Unable to load file. Check the file path.")
+    else:
+        gray_scale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        brightness_grid = compute_grid_brightness(gray_scale)
+        Ascii_result = convert_to_ASCII(brightness_grid)
+        save_ASCII(Ascii_result)
 
 
 
-###### Load file here ######
-image = cv2.imread("c:/Users/.../image.jpg")
-############################
-
-if image is None:
-    raise ValueError("Unable to load file. Check the path.")
-else:
-    gray_scale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # convert file to a grayscale
-    clarte_grille = obt_clarte(gray_scale)
-    Ascii_result = ASCII(clarte_grille)
-    save_ASCII(Ascii_result)
+if __name__ == "__main__":
+    main()
